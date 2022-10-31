c308656993b219d6c3627a35dae16a3ea005a069
scott <thanebhickman@gmail.com+>
2022-10-31 17:24:20 -0600

    rotation fixed

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import gc
+
 import pygame
 import random
 from pygame import KEYDOWN, K_ESCAPE, K_w, K_a, K_s, K_d, K_q, RLEACCEL, K_r, K_e, K_BACKSPACE, K_z, K_c
@@ -13,38 +15,68 @@ newAppleImage = pygame.Surface(appleImage.get_size(), 0, screen)
 newAppleImage.blit(appleImage, (0, 0))
 newAppleImage.set_colorkey((0, 0, 0), RLEACCEL)
 appleImage = newAppleImage
-del newAppleImage
+appleSmallImage = pygame.transform.scale(appleImage, (300, 300))
+del newAppleImage, appleImage
+
+heartImage = pygame.image.load("sprites/heart.png")
+newHeartImage = pygame.Surface(heartImage.get_size(), 0, screen)
+newHeartImage.blit(heartImage, (0, 0))
+newHeartImage.set_colorkey((0, 0, 0), RLEACCEL)
+heartImage = newHeartImage
+heartSmallImage = pygame.transform.scale(heartImage, (300, 300))
+del newHeartImage, heartImage
+
+icecreamImage = pygame.image.load("sprites/icecream.png")
+newIcecreamImage = pygame.Surface(icecreamImage.get_size(), 0, screen)
+newIcecreamImage.blit(icecreamImage, (0, 0))
+newIcecreamImage.set_colorkey((0, 0, 0), RLEACCEL)
+icecreamImage = newIcecreamImage
+icecreamSmallImage = pygame.transform.scale(icecreamImage, (400, 400))
+del newIcecreamImage, icecreamImage
+
+moneyImage = pygame.image.load("sprites/money.png")
+newMoneyImage = pygame.Surface(moneyImage.get_size(), 0, screen)
+newMoneyImage.blit(moneyImage, (0, 0))
+newMoneyImage.set_colorkey((0, 0, 0), RLEACCEL)
+moneyImage = newMoneyImage
+moneySmallImage = pygame.transform.scale(moneyImage, (400, 400))
+del newMoneyImage, moneyImage
+
+musicImage = pygame.image.load("sprites/music.png")
+newMusicImage = pygame.Surface(musicImage.get_size(), 0, screen)
+newMusicImage.blit(musicImage, (0, 0))
+newMusicImage.set_colorkey((0, 0, 0), RLEACCEL)
+musicImage = newMusicImage
+musicSmallImage = pygame.transform.scale(musicImage, (400, 400))
+del newMusicImage, musicImage
 
 # Prepare cache of rotated images
-appleImages = {0: appleImage}
-appleRect = appleImage.get_rect()
+appleSmallImages = {0: appleSmallImage}
+appleSmallRect = appleSmallImage.get_rect()
+
+heartSmallImages = {0: heartSmallImage}
+heartSmallRect = heartSmallImage.get_rect()
+
+icecreamSmallImages = {0: icecreamSmallImage}
+icecreamSmallRect = icecreamSmallImage.get_rect()
+
+moneySmallImages = {0: moneySmallImage}
+moneySmallRect = moneySmallImage.get_rect()
+
+musicSmallImages = {0: musicSmallImage}
+musicSmallRect = musicSmallImage.get_rect()
 
 for angle in range(1, 360):
 
-    rotatedAppleImage = pygame.transform.rotate(appleImage, angle)
-    rotatedAppleRect = rotatedAppleImage.get_rect()
+    appleSmallImages[angle] = pygame.transform.rotate(appleSmallImage, angle)
 
-    if rotatedAppleRect == appleRect:
+    heartSmallImages[angle] = pygame.transform.rotate(heartSmallImage, angle)
 
-        rotatedAppleImage.set_colorkey((0, 0, 0), RLEACCEL)
-        appleImages[angle] = rotatedAppleImage
+    icecreamSmallImages[angle] = pygame.transform.rotate(icecreamSmallImage, angle)
 
-    else:
+    moneySmallImages[angle] = pygame.transform.rotate(moneySmallImage, angle)
 
-        # trim any new transparent pixels around the edges
-
-        newAppleRotatedImage = pygame.Surface(appleRect.size, 0, screen)
-
-        offsetAppleX = (appleRect.width - rotatedAppleRect.width) // 2
-        offsetAppleY = (appleRect.height - rotatedAppleRect.height) // 2
-        newAppleRotatedImage.blit(rotatedAppleImage, (offsetAppleX, offsetAppleY))
-
-        newAppleRotatedImage.set_colorkey((0, 0, 0), RLEACCEL)
-        appleImages[angle] = newAppleRotatedImage
-
-        del newAppleRotatedImage, offsetAppleX, offsetAppleY
-
-del rotatedAppleImage, rotatedAppleRect
+    musicSmallImages[angle] = pygame.transform.rotate(musicSmallImage, angle)
 
 
 class Icon(pygame.sprite.Sprite):
@@ -64,19 +96,26 @@ class Icon(pygame.sprite.Sprite):
 
         if key[K_z]:
             self.angle = (self.angle + self.rotationSpeed) % 360.0
-            self.image = self.images[int(self.angle)]
+            try:
+                self.image = pygame.transform.scale(self.images[int(self.angle)], (self.width, self.height))
+            except KeyError:
+                pass
+
 
         if key[K_c]:
             self.angle = (self.angle - self.rotationSpeed) % 360.0
-            self.image = self.images[int(self.angle)]
+            try:
+                self.image = pygame.transform.scale(self.images[int(self.angle)], (self.width, self.height))
+            except KeyError:
+                pass
 
     def __init__(self):
         super(Icon, self).__init__()
-        # self.surf = pygame.image.load(spriteImages[random.randint(0, 4)]).convert()
-        self.angle = random.randint(1, 360)
+        self.angle = random.randint(1, 359)
 
-        self.images = appleImages
-        self.surf = self.images[round(self.angle)]
+        imagesOptions = [appleSmallImages, heartSmallImages, icecreamSmallImages, moneySmallImages, musicSmallImages]
+        self.images = imagesOptions[random.randint(0, 4)]
+        self.surf = self.images[self.angle]
 
         self.width = random.randint(int(getWindowSize()[1] / 4), int(getWindowSize()[1] / 3))
         self.height = self.width
@@ -96,7 +135,7 @@ class Icon(pygame.sprite.Sprite):
 
         self.speed = random.randint(1, 2)
 
-        rotationalSpeedOptions = [0.1, 0.2, 0.3, 0.4, 0.5]
+        rotationalSpeedOptions = [0.2, 0.3, 0.4, 0.5, 0.6]
         self.rotationSpeed = rotationalSpeedOptions[random.randint(0, 4)]
 
 
@@ -113,6 +152,8 @@ dragMode = False
 # where game starts running
 while running:
 
+    gc.disable()
+
     for event in pygame.event.get():
 
         if event.type == KEYDOWN:
@@ -122,6 +163,9 @@ while running:
 
             # q creates a new sprite
             if event.key == K_q:
+                if len(allSprites) > 25:
+                    allSprites.remove(allSprites.sprites()[0])
+
                 newSprite = Icon()
                 allSprites.add(newSprite)
 
@@ -140,7 +184,7 @@ while running:
             if event.key == K_r:
                 screen.fill((0, 0, 0))
                 for entity in allSprites:
-                    screen.blit(entity.scaled, entity.rect)
+                    screen.blit(entity.image, entity.rect)
 
                 pygame.display.flip()
 
@@ -162,3 +206,5 @@ while running:
         screen.blit(entity.image, entity.rect)
 
     pygame.display.flip()
+
+    gc.collect()
